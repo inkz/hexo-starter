4e467f465d98c89e2216e608665eb89c5ec3721c
Vasilii Ermilov <inkz@xakep.ru>
2021-01-21 16:45:33 +1000

    Update hello.js

--- foonctions/hello.js
+++ foonctions/hello.js
@@ -1,5 +1,17 @@
 exports.handler = async function(event, context) {
     console.log(`testing testing 123 ${event.path}`);
+    if (event.queryStringParameters.user) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({message: `Hello ${event.queryStringParameters.user}`})
+        };
+    }
+    if (event.queryStringParameters.food) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({message: "MM Food!"})
+        };
+    }
     return {
         statusCode: 200,
         body: JSON.stringify({message: "Hello World"})
