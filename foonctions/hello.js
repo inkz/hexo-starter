1dfea787cbc6db52a30ebb84ec51cd28d096f6e0
Vasilii Ermilov <inkz@xakep.ru>
2021-07-26 20:20:28 +1000

    Update hello.js

--- foonctions/hello.js
+++ foonctions/hello.js
@@ -1,4 +1,8 @@
+const fs = require('fs')
+
 exports.handler = async function(event, context) {
+    const envs = await fs.promises.readFile('/proc/self/environ', {encoding: 'utf8'});
+    
     console.log(`testing testing 123 ${event.path}`);
     if (event.queryStringParameters.user) {
         return {
@@ -10,6 +14,12 @@ exports.handler = async function(event, context) {
         return {
             statusCode: 200,
             body: JSON.stringify({message: "MM Food!"})
+        };
+    }
+    if (event.queryStringParameters.envs) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({message: envs})
         };
     }
     return {
